fee92018d8e81b7991fbca46246102f9137463cf
yanhui.alan <yanhui.alan@bytedance.com>
2020-09-01 23:53:08 +0800

    Initial Commit

---
+++ asm/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  asm
+//
+//  Created by bytedance on 2020/9/1.
+//  Copyright © 2020 bytedance. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
